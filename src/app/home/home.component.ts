c066e35440c73660199e3daa95f8d2653cf8f9c1
Joe Giuffrida <giuffridajoe@gmail.com>
2020-03-02 06:54:49 -0500

    custom observable fail

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { Subscription, Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +15,14 @@ export class HomeComponent implements OnInit, OnDestroy {
    this.firstObsSubscription = interval(1000).subscribe(count => {
       console.log(count);
     });
+
+  // const customIntervalObservable = new Observable(observer => {
+  //     let count = 0;
+  //     setInterval(handler: () => {
+  //       observer.next(count);
+  //       count++;
+  //     }, timeout: 1000);
+  //   });
   }
 
   ngOnDestroy() {
